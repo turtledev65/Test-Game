3584e84f11e2bf34752a07ac1ed04c204c5d6e9f
turtledev <turtledev65@gmail.com>
2024-10-12 23:13:49 +0300

    Forgot to add tile tile file

---
+++ src/game/tile.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdlib.h>
+
+#include "util/dynamic-array.h"
+
+typedef enum {
+  TILE_GROUND = 0,
+  TILE_WALL_VERTICAL,
+  TILE_WALL_HORIZONTAL,
+} TileType;
+
+typedef struct {
+  union {
+    size_t col;
+    size_t x;
+  };
+  union {
+    size_t row;
+    size_t z;
+  };
+  TileType type;
+} Tile;
+
+typedef DYN_ARR(Tile) Tiles;
